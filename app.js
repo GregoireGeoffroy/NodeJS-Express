e64ea1ddc9f64e58b33dc060f09737370196dbe4
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-24 15:16:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+  ];
+  
+  const users = ["Rose", "Cake", "Biff"];
+  
+  app.get("/", (req, res) => {
+    res.render("index", { links: links, users: users });
+  });
+  
 const PORT = 3000;
-app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
